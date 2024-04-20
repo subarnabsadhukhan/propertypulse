100e44c88b9e0ff9fcca78df588c18c40d28395c
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2024-04-20 16:38:04 +0530

    creating new property data in MongoDB from user form Input

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import getSessionUser from "@/utils/getSessionUser";
 
 export const GET = async () => {
   try {
@@ -14,6 +15,75 @@ export const GET = async () => {
     console.log(error);
 
     return new Response("Internal Server Error", {
+      status: 500,
+    });
+  }
+};
+
+export const POST = async (request) => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response(JSON.stringify({ message: "Unauthorized" }), {
+        status: 401,
+      });
+    }
+
+    const { userId } = sessionUser;
+
+    const formData = await request.formData();
+
+    // Access All Values from Amenities and Images
+    const amenities = formData.getAll("amenities");
+    const images = formData
+      .getAll("images")
+      .filter((image) => image.name !== "");
+
+    // Create PropertyData Object for Database
+    const propertyData = {
+      name: formData.get("name"),
+      type: formData.get("type"),
+      description: formData.get("description"),
+      location: {
+        street: formData.get("location.street"),
+        city: formData.get("location.city"),
+        state: formData.get("location.state"),
+        zipcode: formData.get("location.zipcode"),
+      },
+      beds: formData.get("beds"),
+      baths: formData.get("baths"),
+      square_feet: formData.get("square_feet"),
+      rates: {
+        weekly: formData.get("rates.weekly"),
+        monthly: formData.get("rates.monthly"),
+        nightly: formData.get("rates.nightly"),
+      },
+      amenities,
+      seller_info: {
+        name: formData.get("seller_info.name"),
+        email: formData.get("seller_info.email"),
+        phone: formData.get("seller_info.phone"),
+      },
+      owner: userId,
+      // images,
+    };
+
+    const newProperty = new Property(propertyData);
+    await newProperty.save();
+
+    return Response.redirect(
+      `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
+    );
+
+    // return new Response(JSON.stringify({ message: "Not implemented" }), {
+    //   status: 200,
+    // });
+  } catch (error) {
+    console.error(error);
+    return new Response("Failed to add property", {
       status: 500,
     });
   }
---
+++ utils/getSessionUser.js
@@ -0,0 +1,22 @@
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+
+const getSessionUser = async () => {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+      return null;
+    }
+
+    return {
+      user: session.user,
+      userId: session.user.id,
+    };
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export default getSessionUser;
